4bef8653309c6d1fc2afe3ff00d314e3cdd73938
fjovanovic <filipjovanovic98@gmail.com>
2023-12-16 13:23:54 +0100

    Added support for file logging

---
+++ utils/utils.py
@@ -0,0 +1,19 @@
+import logging
+import logging.handlers
+
+
+def setup_file_logging() -> None:
+    logger = logging.getLogger('')
+    logger.setLevel(logging.INFO)
+
+    handler = logging.handlers.RotatingFileHandler(
+        filename='discord.log',
+        encoding='utf-8',
+        maxBytes=32 * 1024 * 1024,
+        backupCount=5,
+    )
+
+    dt_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
